ca41c85435b429a2f27a4a7fde4a1f942f53ded4
86sea <25084409+86sea@users.noreply.github.com>
2023-06-04 11:17:36 +0200

    my astar

--- client/src/pathfinding.rs
+++ client/src/pathfinding.rs
@@ -1,12 +1,16 @@
-use std::collections::BinaryHeap;
+use std::{collections::BinaryHeap, sync::Mutex};
 
 use bevy::{
     prelude::*,
     utils::{HashMap, HashSet},
 };
+use lazy_static::lazy_static;
 use lib::components::{FloorTile, Path, Tile, Untraversable};
 
-#[derive(Clone, Eq, PartialEq, PartialOrd, Debug)]
+lazy_static! {
+    static ref GOAL: Mutex<(u32, u32)> = Mutex::new((0, 0));
+}
+#[derive(Copy, Hash, Clone, Eq, PartialEq, PartialOrd, Debug)]
 pub struct Node {
     pos: Tile,
     g_cost: u32,
@@ -25,20 +29,14 @@ impl Node {
         let h_cost = dx + dz;
         let f_cost = g_cost + h_cost;
         let parent = None;
-
+        let mut goal = GOAL.lock().unwrap();
+        *goal = (10, 10);
         Node {
             pos: *pos,
             g_cost,
             h_cost,
             f_cost,
             parent,
-        }
-    }
-    fn success(&self, path: &Path) -> bool {
-        if self.pos == path.destination {
-            true
-        } else {
-            false
         }
     }
 }
@@ -48,18 +46,24 @@ impl Ord for Node {
     }
 }
 
+fn success(pos: (u32, u32)) -> bool {
+    if let Ok(goal) = GOAL.into_inner() {
+        if goal == pos {
+            true
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
 pub fn get_neighbours(
     path_info: &Path,
     current: &Tile,
     tiles: &Query<&Tile, (With<FloorTile>, Without<Untraversable>)>,
-) -> Vec<Node> {
-    let mut neighbours: Vec<Node> = vec![];
+) -> BinaryHeap<Node> {
+    let mut neighbours: BinaryHeap<Node> = BinaryHeap::new();
     for tile in tiles.iter() {
-        
-        //exlucde current tile?
-        if tile == current {
-            continue 
-        }
         //North
         if tile.cell.0 == current.cell.0 + 1 && tile.cell.2 == current.cell.2 {
             neighbours.push(Node::new(tile, path_info));
@@ -103,35 +107,47 @@ pub fn get_neighbours(
     neighbours
 }
 
-fn success(path: &Path) -> bool {
-    false
-}
+//pub fn a_star(
+//path_query: Query<&Path, Changed<Path>>,
+//tiles: Query<&Tile, (With<FloorTile>, Without<Untraversable>)>,
+//) {
+//if let Ok(path_info) = path_query.get_single() {
+//let mut open_set: BinaryHeap<Node> = BinaryHeap::new();
+//let mut open_check: HashSet<Node> = HashSet::new();
+//let mut closed_set: HashSet<Node> = HashSet::new();
+//let mut came_from: HashMap<Tile, Option<Tile>> = HashMap::new();
+//open_set.push(Node::new(&path_info.origin, path_info));
+//open_check.insert(Node::new(&path_info.origin, path_info));
 
-pub fn a_star(
-    path_query: Query<&Path, Changed<Path>>,
-    tiles: Query<&Tile, (With<FloorTile>, Without<Untraversable>)>,
-) {
-    if let Ok(path_info) = path_query.get_single() {
-        //let sucessor_nodes = successors(&path, tiles);
-        let mut open_set: BinaryHeap<Node> = BinaryHeap::new();
-        let mut closed_set: HashSet<Tile> = HashSet::new();
-        let mut came_from: HashMap<Tile, Option<Tile>> = HashMap::new();
-        open_set.push(Node::new(&path_info.origin, path_info));
+//while let Some(current) = open_set.pop() {
+//println!("current {:?}", current);
+//closed_set.insert(current);
+//if current.pos == path_info.destination {
+//let mut path = vec![current.pos];
+//let mut current = current.pos;
 
-        while let Some(node) = open_set.pop() {
-            if node.pos == path_info.destination {
-                let mut path = vec![node.pos];
-                let mut current = node.pos;
+//while let Some(Some(prev)) = came_from.get(&current) {
+//path.push(*prev);
+//current = *prev;
+//}
 
-                while let Some(Some(prev)) = came_from.get(&current) {
-                    path.push(*prev);
-                    current = *prev;
-                }
-
-                path.reverse();
-                //println!("path: {:?}", path);
-            }
-            let neighbours: Vec<Node> = get_neighbours(&path_info, &node.pos, &tiles);
-        }
-    }
-}
+//path.reverse();
+//println!("path: {:?}", path);
+//}
+//let mut neighbours: BinaryHeap<Node> = get_neighbours(&path_info, &current.pos, &tiles);
+//while let Some(mut neighbour) = neighbours.pop() {
+//if closed_set.contains(&neighbour) {
+//continue;
+//}
+//if !open_check.contains(&neighbour) {
+//neighbour.parent = Some(current.pos);
+//came_from.insert(neighbour.pos, Some(current.pos));
+//open_set.push(neighbour);
+//open_check.insert(neighbour);
+//break;
+//}
+//}
+//}
+//}
+//A
+//}
